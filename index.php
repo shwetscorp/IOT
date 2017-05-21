0402650c6c89e1f4e9ea2188a9ca274819ae168f
Shwetank Yadav <shwet.scorp007@gmail.com>
2017-05-21 22:38:16 +0530

    Update 5

--- index.php
+++ index.php
@@ -147,6 +147,7 @@ $file_name="programs/PIR/name.php";
 <html>
   <head lang="en">
   <meta charset="UTF-8">
+  <title>IOT Sulutions</title>
   <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
 
 <!--css-->
